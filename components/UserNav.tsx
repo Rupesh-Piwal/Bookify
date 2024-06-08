02f6a7c5edd47d44d357501c45039990b77fc682
rupesh <rpiwal02@gmail.com>
2024-06-09 01:48:32 +0530

    auth implementation

--- components/UserNav.tsx
+++ components/UserNav.tsx
@@ -5,23 +5,48 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-function UserNav() {
+async function UserNav() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
         <div className="rounded-full border px-2 py-2 lg:px-4 lg:py-2 flex items-center gap-x-3">
           <MenuIcon className="w-6 h-6 lg:w-5 lg:h-5" />
           <img
-            src="https://images.rawpixel.com/image_social_square/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcGYtaWNvbjQtamlyMjA2NC1wb3ItbC5qcGc.jpg"
+            src={
+              user?.picture ??
+              "https://images.rawpixel.com/image_social_square/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcGYtaWNvbjQtamlyMjA2NC1wb3ItbC5qcGc.jpg"
+            }
             alt="user-image"
             className="rounded-full h-8 w-8 hidden lg:block"
           />
         </div>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-[200px]">
-        <DropdownMenuItem className="cursor-pointer">Register</DropdownMenuItem>
-        <DropdownMenuItem className="cursor-pointer">Login</DropdownMenuItem>
+        {user ? (
+          <>
+            <DropdownMenuItem className="cursor-pointer">
+              <LogoutLink>Logout</LogoutLink>
+            </DropdownMenuItem>
+          </>
+        ) : (
+          <>
+            <DropdownMenuItem className="cursor-pointer">
+              <RegisterLink className="w-full">Register</RegisterLink>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="cursor-pointer">
+              <LoginLink className="w-full">Login</LoginLink>
+            </DropdownMenuItem>
+          </>
+        )}
       </DropdownMenuContent>
     </DropdownMenu>
   );
