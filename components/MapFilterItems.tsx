f31d6daa288a7f892c40dfef975d35b5fee22cc9
rupesh <rpiwal02@gmail.com>
2024-06-11 14:58:48 +0530

    filter pathname

--- components/MapFilterItems.tsx
+++ components/MapFilterItems.tsx
@@ -1,12 +1,39 @@
+"use client";
+
 import { categoryItems } from "@/app/lib/categoryItems";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useCallback } from "react";
 
 const MapFilterItems = () => {
+  const searchParams = useSearchParams();
+  const search = searchParams.get("filter");
+  const pathname = usePathname();
+
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set(name, value);
+      return params.toString();
+    },
+    [searchParams]
+  );
+
   return (
-    <div>
+    <div className="flex gap-x-10 mt-5 overflow-x-scroll no-scrollbar">
       {categoryItems.map((item) => (
-        <Link key={item.id} href="">
+        <Link
+          key={item.id}
+          href={pathname + "?" + createQueryString("filter", item.name)}
+          className={cn(
+            search === item.name
+              ? "border-b-2 border-black pb-2 flex-shrink-0"
+              : "opacity-70 flex-shrink-0",
+            "flex flex-col gap-y-3 items-center"
+          )}
+        >
           <div>
             <Image
               src={item.imageUrl}
@@ -16,6 +43,7 @@ const MapFilterItems = () => {
               height={24}
             />
           </div>
+          <p className="text-xs font-medium">{item.title}</p>
         </Link>
       ))}
     </div>
